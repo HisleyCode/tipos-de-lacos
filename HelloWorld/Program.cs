97e2b2e5fc5b360b72654a203c17f6d5b707ee8a
HisleyCode <hisley89@gmail.com>
2022-05-14 14:19:46 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Variavel com o meu nome
+string myName = "Hisley Augusto";
+int idade = 22;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Seja bem vindo ao mundo do Csharp " + myName + " é um prazer te-lô aqui conosco");
+Console.WriteLine("Hoje estou com "+ idade + " anos de vida.");
---
+++ TipoEvariaveis/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace TiposEvariaveis
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            //Laços interativos => estruturad que repetem um bloco de código, determinado número de vezes
+
+            //Laço for
+            Console.WriteLine("For");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Valor de i é: " + i);
+            }
+
+            //While
+            Console.WriteLine("\r\nWhile");
+
+            int contador = 3;
+            while(contador < 10)
+            {
+                Console.WriteLine(contador);
+                contador++;
+            }
+
+            //Do...While
+            // Execução garantida pelo menos uma vez! (executando o bloco primeiro e depois faz o teste da condição)
+            Console.WriteLine("\r\nDo...While");
+            
+            double j = 2;
+            do
+            {
+                Console.WriteLine(j);
+                j = j * 2;
+            } while (j < 100);
+
+            //Foreach
+            //Percorre todos os elementos de um conjunto
+            Console.WriteLine("\r\nForeach");
+
+            int[] conjunto = { 1, 2, 3, 4};
+            foreach(int numero in conjunto)
+            {
+                Console.WriteLine("Esse elemento do conjunto tem valor: " + numero);
+            }
+        }
+    }
+}
